d862eefd66504bb93657d9834536d36181e3c32a
BjornFrierPedersen <bjornapedersen@yahoo.dk>
2023-09-26 20:57:25 +0200

    Update GaveListExtensions.cs

--- Skatteberegner/GaveListExtensions.cs
+++ Skatteberegner/GaveListExtensions.cs
@@ -36,7 +36,7 @@ public static class GaveListExtensions
             return juleGave == null ? sum : UdregnJulegaveBeskatningFraSum(juleGave, sum);
         
         Console.WriteLine(
-            $"Der er ikke givet nogen julegave og vi er under det fradragsberettigede beløb og det skattepligtige beløb er derfor: {sum}");
+            $"Der er ikke givet nogen julegave og vi er under det fradragsberettigede beløb og det skattepligtige beløb er derfor: {sum} DKK");
         return sum;
 
     }
@@ -46,17 +46,19 @@ public static class GaveListExtensions
         if (julegave.SkattepligtigGraenseOverskredet && _fradragsberettigetBeloeb < julegave.Pris + sum)
         {
             var skattepligtigtBeloeb = sum + julegave.Pris;
-            Console.WriteLine($"Både julegavens skattepligtige beløb og det fradragsberettigede beløb er overskredet, det skattepligtige beløb er derfor: {skattepligtigtBeloeb}");
+            Console.WriteLine($"Både julegavens skattepligtige beløb og det fradragsberettigede beløb er overskredet, det skattepligtige beløb er derfor: {skattepligtigtBeloeb} DKK");
             return skattepligtigtBeloeb;
         }
         
         if (!julegave.SkattepligtigGraenseOverskredet && _fradragsberettigetBeloeb < julegave.Pris + sum)
         {
             Console.WriteLine(
-                $"Julegaven er under den skattepligtige grænse, men det samlede beløb overskrider det fradragsberettigede beløb, det skattepligtige beløb er derfor: {sum}");
+                $"Julegaven er under den skattepligtige grænse, men det samlede beløb overskrider det fradragsberettigede beløb, det skattepligtige beløb er derfor: {sum} DKK");
             return sum;
         }
 
+        Console.WriteLine("Det fradragsberettigede beløb på 1200 DKK er ikke overskredet og det skattepligtige beløb er derfor 0.0 DKK");
+        
         return 0.0;
     }
     
